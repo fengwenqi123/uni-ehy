bc43a075e54650e1c5a62f47e570957edce419d2
fengwenqi123 <1318756239@qq.com>
2019-10-15 09:59:51 +0800

    1015

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,11 +1,18 @@
 <template>
 	<view>
-		我的
+		<navigation-bar></navigation-bar>
+		<view> 状态栏下的文字 </view>
 	</view>
 </template>
 
 <script>
+	import navigationBar from '@/components/navigation-bar/index.vue'
+	export default {
+		name: 'index',
+		components:{
+			navigationBar
+		}
+	}
 </script>
-
-<style>
+<style scoped lang="scss">
 </style>
