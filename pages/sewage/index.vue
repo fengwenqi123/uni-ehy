b39ae6f3257b28554a29173b2fa77efcb8a8b73a
周新宇 <392298118@qq.com>
2019-10-17 09:04:37 +0800

    zxy

--- pages/sewage/index.vue
+++ pages/sewage/index.vue
@@ -1,73 +1,115 @@
 <template>
 	<view class="sewage">
-		<div>
+		<div class="top">
 			<div>
-				<image class="img" src="../../static/img/ships.png"></image>
-				<p>{{ shipName }}</p>
+				<div>
+					<image class="img" src="../../static/img/ships.png"></image>
+					<p>{{ shipName }}</p>
+				</div>
+				<div>
+					<picker mode="selector" :range="shipList" @change="selectShip"><image class="img1" src="../../static/img/ships.png"></image></picker>
+				</div>
 			</div>
-			<div>
-				<picker mode="selector" :range="shipList" :value="shipName" @change="selectShip"><image class="img1" src="../../static/img/ships.png"></image></picker>
-			</div>
+			<div><span>排污积分：0 ></span></div>
+			<div @click="getScanCode"><image class="imgCode" src="../../static/img/sys.png"></image></div>
 		</div>
-		<div><span>排污积分：0 ></span></div>
-		<div @click="getScanCode"><image class="imgCode" src="../../static/img/sys.png"></image></div>
 		<div class="cell">
 			<div>
 				<image class="imgCell" src="../../static/img/ywshs.png"></image>
 				<span>油污水回收记录</span>
 			</div>
-			<div><image class="imgCell" src="../../static/img/Path.png"></image></div>
+			<div><image class="imgCell" src="../../static/img/i-right.png"></image></div>
 		</div>
 		<div class="cell">
 			<div>
 				<image class="imgCell" src="../../static/img/shlj.png"></image>
 				<span>生活垃圾回收记录</span>
 			</div>
-			<div><image class="imgCell" src="../../static/img/Path.png"></image></div>
+			<div><image class="imgCell" src="../../static/img/i-right.png"></image></div>
 		</div>
 		<div class="cell">
 			<div>
 				<image class="imgCell" src="../../static/img/shws.png"></image>
 				<span>生活污水回收记录</span>
 			</div>
-			<div><image class="imgCell" src="../../static/img/Path.png"></image></div>
+			<div><image class="imgCell" src="../../static/img/i-right.png"></image></div>
 		</div>
 		<div class="cell">
 			<div>
 				<image class="imgCell" src="../../static/img/hsdwz.png"></image>
 				<span>回收点位置查询</span>
 			</div>
-			<div><image class="imgCell" src="../../static/img/Path.png"></image></div>
+			<div><image class="imgCell" src="../../static/img/i-right.png"></image></div>
 		</div>
 		<div class="cell">
 			<div>
 				<image class="imgCell" src="../../static/img/wfsa.png"></image>
 				<span>无法上岸问题上班</span>
 			</div>
-			<div><image class="imgCell" src="../../static/img/Path.png"></image></div>
+			<div><image class="imgCell" src="../../static/img/i-right.png"></image></div>
 		</div>
 	</view>
 </template>
 
 <script>
+import { boatList, recoveryInfo } from '@/api/sewage.js';
 export default {
 	data() {
 		return {
-			shipName: '浙萧山货32132',
-			shipList: ['浙萧山货32132', '浙诸暨货3123', '浙萨达多']
+			shipName: '',
+			code: '',
+			shipList: [],
+			codeInfo: null
 		};
 	},
+	created() {
+		this.list();
+	},
 	methods: {
-		selectShip(value) {
-			console.log(value);
+		list() {
+			boatList(2).then(response => {
+				console.log(response);
+				this.shipList = response.data.map(item => item.shipName);
+				this.shipName = this.shipList[0];
+			});
+		},
+		selectShip(e) {
+			console.log(e.detail.value);
+			this.shipName = this.shipList[e.detail.value];
 		},
 		getScanCode() {
-			uni.scanCode({
-				success: function(res) {
-					console.log('条码类型：' + res.scanType);
-					console.log('条码内容：' + res.result);
-				}
-			});
+			// uni.scanCode({
+			// 	success: function(res) {
+			// 		console.log('条码类型：' + res.scanType);
+			// 		console.log('条码内容：' + res.result);
+			// 		// this.code = res.result;
+			// 		this.getRecoveryInfo();
+			// 	}
+			// });
+			this.getRecoveryInfo();
+		},
+		getRecoveryInfo() {
+			// recoveryInfo(this.shipName, this.code).then(response => {
+			// this.codeInfo=response.data
+			this.code = '123123213';
+			this.codeInfo = JSON.stringify({ shipName: 'sasasas', siteName: '花式科技', type: 3, attribute: 2, address: '湖州市港航管理局',name:"123213123" });
+			switch (2) {
+				case 1:
+					uni.navigateTo({
+						url: `/pages/sewage/lifeWater/index?code=${this.code}&info=${this.codeInfo}`
+					});
+					break;
+				case 2:
+					uni.navigateTo({
+						url: `/pages/sewage/oil/index?code=${this.code}&info=${this.codeInfo}`
+					});
+					break;
+				default:
+					uni.navigateTo({
+						url: `/pages/sewage/lifeRubbish/index?code=${this.code}&info=${this.codeInfo}`
+					});
+			}
+			// });
 		}
 	}
 };
@@ -76,60 +118,65 @@ export default {
 <style lang="scss" scoped>
 .sewage {
 	width: 100%;
-	div:nth-child(1),
-	div:nth-child(2) {
-		padding: 10rpx;
-		width: 100%;
-		background-color: #0faeff;
-		display: flex;
-		height: 60rpx;
-		// align-items: center;
-		.img {
-			width: 60rpx;
-			height: 60rpx;
-			float: left;
-		}
-		p{
-			line-height: 60rpx;
-			color:#fff;
-			font-size:30rpx;
-		}
+	.top {
+		background-color: #007aff;
+		div:nth-child(1),
 		div:nth-child(2) {
+			padding: 10rpx;
+			width: 100%;
 			display: flex;
-			justify-content: flex-end;
-			margin-right: 20rpx;
-			.img1 {
-				float: right;
+			height: 60rpx;
+			align-items: center;
+			.img {
 				width: 60rpx;
 				height: 60rpx;
+				float: left;
+			}
+			p {
+				line-height: 60rpx;
+				color: #fff;
+				margin-left: 20rpx;
+				font-size: 40rpx;
+			}
+			div:nth-child(2) {
+				display: flex;
+				justify-content: flex-end;
+				margin-right: 20rpx;
+				.img1 {
+					float: right;
+					width: 60rpx;
+					height: 60rpx;
+				}
+			}
+			span {
+				color: #fff;
+				display: inline-block;
+				margin-left: 10rpx;
+				height: 80rpx;
+				font-size: 35rpx;
+				line-height: 80rpx;
 			}
 		}
-		span {
-			color: #fff;
-			display: inline-block;
-			margin-left: 10rpx;
-			height: 80rpx;
-			font-size: 35rpx;
-			line-height: 80rpx;
-		}
-	}
-	div:nth-child(3) {
-		text-align: center;
-		background-color: #0faeff;
-		height: 300rpx;
-		padding: 50rpx;
-		.imgCode {
-			height: 200rpx;
-			width: 200rpx;
+		div:nth-child(3) {
+			text-align: center;
+			height: 300rpx;
+			padding: 50rpx;
+			.imgCode {
+				height: 200rpx;
+				width: 200rpx;
+			}
 		}
 	}
 	.cell {
 		width: 100%;
 		background-color: #fff;
 		display: flex;
+		height: 80rpx;
 		padding: 10rpx 0;
 		justify-content: space-between;
 		div {
+			display: flex;
+			padding: 0 10rpx;
 			align-items: center;
 			background-color: #fff;
 			.imgCell {
@@ -138,6 +185,7 @@ export default {
 			}
 			span {
 				color: #333;
+				margin-left: 10px;
 				font-size: 30rpx;
 			}
 		}
@@ -148,7 +196,7 @@ export default {
 			.imgCell {
 				width: 20rpx;
 				height: 20rpx;
-				margin-right:20rpx;
+				margin-right: 20rpx;
 			}
 		}
 	}
---
+++ pages/sewage/oil/index.vue
@@ -0,0 +1,174 @@
+<template>
+	<view class="sewage">
+		<div class="top">
+			<p>油污水回收船</p>
+			<p>
+				<span>{{ recoveryInfo.shipName }}</span>
+			</p>
+		</div>
+		<div class="content">
+			<p>
+				<span>回收类型</span>
+				<span>油污水回收</span>
+			</p>
+			<p>
+				<span>船舶名称</span>
+				<span>{{ recoveryInfo.shipName }}</span>
+			</p>
+			<p>
+				<span>回收点</span>
+				<span>{{ recoveryInfo.siteName }}</span>
+			</p>
+			<p>
+				<span>排放口</span>
+				<span>{{ recoveryInfo.name }}</span>
+			</p>
+			<p>
+				<span>排放口类型</span>
+				<span>{{ recoveryInfo.attribute === 1 ? '智能' : '普通' }}</span>
+			</p>
+			<p v-if="recoveryInfo.attribute !== 1">
+				<span>排放容积(L)</span>
+				<input  v-model="amount" placeholder="请输入排放容积"></input>
+			</p>
+		</div>
+		<div class="bottom" v-if="recoveryInfo.attribute === 1"><button :loading="isLoading" @click="submit">确认开始排放</button></div>
+		<div class="bottom" v-if="recoveryInfo.attribute !== 1"><button :loading="isLoading" @click="submit1">确认排放</button></div>
+	</view>
+</template>
+
+<script>
+import { discharge } from '@/api/sewage.js';
+export default {
+	onLoad(option) {
+		const item = option;
+		this.recoveryInfo = JSON.parse(item.info);
+		console.log(this.recoveryInfo);
+		this.recoveryCode = item.code;
+		console.log(this.recoveryCode);
+	},
+	data() {
+		return {
+			value: '请输入容积',
+			isLoading: false,
+			amount: '',
+			recoveryCode: '',
+			recoveryInfo: {}
+		};
+	},
+	methods: {
+		submit() {
+			this.isLoading = true;
+			const obj = {
+				type: 2,
+				shipName: this.recoveryInfo.shipName,
+				code: this.recoveryCode,
+				amount: this.amount,
+				orderWay: 1
+			};
+			uni.navigateTo({
+				url: `/pages/sewage/success/index?type=2&attr=1`
+			});
+			discharge(obj).then(response => {
+				Toast.success({
+					message: response.msg,
+					duration: 2000
+				});
+				setTimeout(() => {
+					uni.navigateTo({
+						url: `/pages/sewage/success/index?type=2&attr=${this.recoveryInfo.attribute}`
+					});
+				}, 2000);
+			});
+		},
+		submit1() {
+			console.log('是是是', this.amount);
+			if (this.amount === '') {
+				uni.showToast({
+					icon: 'none',
+					title: '请填写排放容积'
+				});
+				return;
+			}
+			const obj = {
+				type: 2,
+				shipName: this.recoveryInfo.shipName,
+				code: this.recoveryCode,
+				amount: this.amount,
+				orderWay: 1
+			};
+			uni.navigateTo({
+				url: `/pages/sewage/success/index?type=2&attr=1`
+			});
+			discharge(obj).then(response => {
+				Toast.success({
+					message: response.msg,
+					duration: 2000
+				});
+				setTimeout(() => {
+					uni.navigateTo({
+						url: `/pages/sewage/success/index?type=2&attr=${this.recoveryInfo.attribute}`
+					});
+				}, 2000);
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.sewage {
+	width: 100%;
+	.top {
+		display: flex;
+		justify-content: space-between;
+		padding: 30rpx;
+		background-color: #007aff;
+		p {
+			font-size: 28rpx;
+			color: #fff;
+		}
+		p:nth-child(1) {
+			width: 30%;
+			line-height: 35rpx;
+		}
+		p:nth-child(2) {
+			width: 50%;
+			line-height: 35rpx;
+			span {
+				text-align: right;
+				display: block;
+			}
+		}
+	}
+	.content {
+		background: #fff;
+		p {
+			height: 80rpx;
+			padding: 0 20rpx;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			span:nth-child(1) {
+				color: #888;
+			}
+			span:nth-child(2) {
+				color: #333;
+			}
+			input {
+				text-align: right;
+			}
+		}
+	}
+	.bottom {
+		width: 100%;
+		padding: 5% 0;
+		button {
+			margin: 0 auto;
+			width: 90%;
+			background-color: #007aff;
+			color: #ffffff;
+		}
+	}
+}
+</style>
